6605fe5264cf7c8d5b358246beac74accac8456f
elias90000 <eliasehsetu2847@gmail.com>
2022-03-19 00:27:13 +0300

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int i;
+	int first_number;
+        int second_number;
+	int result;
+	
+	i = 0;
+	result = 0;
+	while (i < 10)
+	{
+		while (result <= 14)
+		{
+			if (result < 10)
+			{
+				second_number = result;
+			}
+			else
+			{
+				first_number = result / 10;
+				second_number = result % 10;
+				putchar (first_number + '0');
+						}
+_putchar (second_number + '0');
+
+result++;
+		}
+		i++;
+		result = 0;
+		_putchar ('\n');
+	}
+}
+
