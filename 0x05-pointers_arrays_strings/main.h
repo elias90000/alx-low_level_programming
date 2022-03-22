5b09cff26a7d04d4e89693d8b7c1f6f703adb7e1
elias90000 <eliasehsetu2847@gmail.com>
2022-03-22 22:00:44 +0300

    0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+int putchar_(char c);
+void swap_int(int *a, int *b);
+int strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *S);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atol(char *s);
