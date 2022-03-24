0814ef51659248014b2899575aa7b108183721e3
elias90000 <eliasehsetu2847@gmail.com>
2022-03-24 12:31:05 +0300

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	i = 0;
+	while (dest[i] !- '\0')
+	{
+		i++;
+	}
+	i = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
