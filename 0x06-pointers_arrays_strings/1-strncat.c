ca1788b36aca62c9d38b466064f04837765904e1
elias90000 <eliasehsetu2847@gmail.com>
2022-03-24 14:31:05 +0300

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,5 @@
 #include "main.h"
 
-/**
- * _strncat - concatenates two strings
- * @dest: A pointer to a character that will be changed
- * @src: Apointer to a character that will be changed
- * @n: value
- * Return: dest
- */
-
 char *_strcat(char *dest, char *src, int n);
 {
  int i,j;
